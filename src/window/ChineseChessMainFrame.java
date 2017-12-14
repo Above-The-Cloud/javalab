5d0d37074ad87ef17b75db936d45ee7d530a9dcf
Michael <yiwangchunyu@163.com>
2017-12-15 02:17:49 +0800

    version 1.0

--- src/window/ChineseChessMainFrame.java
+++ src/window/ChineseChessMainFrame.java
@@ -28,8 +28,8 @@ public class ChineseChessMainFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel Pane1;
-	private JPanel Pane2;
-	private JPanel Pane3;
+	private HomePanel Pane2;
+	private HomePanel Pane3;
 	private JPanel Pane4;
 	
 	static public InformationBoard InfBoard;
@@ -74,7 +74,7 @@ public class ChineseChessMainFrame extends JFrame {
 		//设置标题
 		this.setTitle("中国象棋");
 		//设置窗口大小
-		this.setBounds(0, 0, 1366, 768);
+		this.setBounds(0, 0, 1400, 800);
 		//设置窗口不可改变大小
 		this.setResizable(false);
 		//设置默认关闭
@@ -95,29 +95,28 @@ public class ChineseChessMainFrame extends JFrame {
 		
 		//添加背景图片
 		JLabel BackGround = new JLabel("");
-		BackGround.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ChineseChessMainFrame.class.getResource("/imageLibary/background.png"))));
-		BackGround.setBounds(0, 0, 1366, 768);
+		BackGround.setIcon(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ChineseChessMainFrame.class.getResource("/imageLibary/bgd.jpg"))));
+		BackGround.setBounds(0, 0, 1400, 800);
 		//添加背景图片的关键语句
 		this.getLayeredPane().add(BackGround, new Integer(Integer.MIN_VALUE)); 
 
-		
 		//初始化4个JPanel
 		Pane1 = new JPanel();
-		Pane2 = new JPanel();
-		Pane3 = new JPanel();
+		Pane2 = new HomePanel();
+		Pane3 = new HomePanel();
 		
 		//设置4个JPanel的位置和共同属性
-		Pane1.setBounds(0, 0, 1366, 768);
+		Pane1.setBounds(0, 0, 1400, 800);
 		Pane1.setOpaque(false);
 		Pane1.setVisible(true);
 		Pane1.setLayout(null);
-		Pane2.setBounds(0, 0, 1366, 768);
+		Pane2.setBounds(750,70,400,322);
 		Pane2.setOpaque(false);
-		Pane2.setVisible(false);
+		Pane2.setVisible(true);
 		Pane2.setLayout(null);
-		Pane3.setBounds(0, 0, 1366, 768);
+		Pane3.setBounds(800,370,400,322);
 		Pane3.setOpaque(false);
-		Pane3.setVisible(false);
+		Pane3.setVisible(true);
 		Pane3.setLayout(null);
 
 		
@@ -132,21 +131,65 @@ public class ChineseChessMainFrame extends JFrame {
 		//设置Canvas位置和大小
 		MyCanvas.setBounds(DefaultSet.CanvasPosX, DefaultSet.CanvasPosY, 661, 728);
 		//为Canvas传递数据
-		MyCanvas.SendData(this.MyBoarder, Toolkit.getDefaultToolkit().getImage(ChineseChessMainFrame.class.getResource("/imageLibary/background.png")), DefaultSet.CanvasPosX, DefaultSet.CanvasPosY, DefaultSet.CanvasPosX+661, DefaultSet.CanvasPosY+728);
+		//MyCanvas.SendData(this.MyBoarder, Toolkit.getDefaultToolkit().getImage(ChineseChessMainFrame.class.getResource("/imageLibary/bgd.jpg")), DefaultSet.CanvasPosX, DefaultSet.CanvasPosY, DefaultSet.CanvasPosX+661, DefaultSet.CanvasPosY+728);
 		MyCanvas.repaint();
 		MyCanvas.addMouseListener(new ChessPieceClick(userInfo));
 		Pane1.add(MyCanvas);
-		
+		MyCanvas.setOpaque(false);
 		//对Pane1添加信息栏
 		InfBoard = new InformationBoard();
-		InfBoard.setBounds(1011, 50, 394, 481);
+		InfBoard.setBounds(1100, 50, 394, 481);
 		Pane1.add(InfBoard);
 		
 		InfBoard.AddLog("红方执子");
+		//添加用户信息栏1
+		JLabel user1name = new JLabel("红方:"+userInfo[0].getUserName());
+		user1name.setBounds(161,30,300,40);
+		user1name.setFont(new Font("华文隶书",Font.CENTER_BASELINE,30));
+		Pane2.add(user1name);
+		JLabel user1win = new JLabel("赢："+userInfo[0].getNumWin()+"局");
+		user1win.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user1win.setBounds(161,70,300,40);
+		Pane2.add(user1win);
+		JLabel user1lose = new JLabel("输："+userInfo[0].getNumLose()+"局");
+		user1lose.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user1lose.setBounds(161,110,300,40);
+		Pane2.add(user1lose);
+		JLabel user1peace = new JLabel("平："+userInfo[0].getNumPeace()+"局");
+		user1peace.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user1peace.setBounds(161,150,300,40);
+		Pane2.add(user1peace);
+		JLabel user1rate = new JLabel("胜率："+String.format("%.1f", (userInfo[0].getNumWin()*100.0/(userInfo[0].getNumWin()+userInfo[0].getNumLose()+userInfo[0].getNumPeace())))+"%");
+		user1rate.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user1rate.setBounds(161,190,300,40);
+		Pane2.add(user1rate);
+		
+		//添加用户信息栏2
+		JLabel user2name = new JLabel("黑方:"+userInfo[1].getUserName());
+		user2name.setBounds(161,30,300,40);
+		user2name.setFont(new Font("华文隶书",Font.CENTER_BASELINE,30));
+		Pane3.add(user2name);
+		JLabel user2win = new JLabel("赢："+userInfo[1].getNumWin()+"局");
+		user2win.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user2win.setBounds(161,70,300,40);
+		Pane3.add(user2win);
+		JLabel user2lose = new JLabel("输："+userInfo[1].getNumLose()+"局");
+		user2lose.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user2lose.setBounds(161,110,300,40);
+		Pane3.add(user2lose);
+		JLabel user2peace = new JLabel("平："+userInfo[1].getNumPeace()+"局");
+		user2peace.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user2peace.setBounds(161,150,300,40);
+		Pane3.add(user2peace);
+		JLabel user2rate = new JLabel("胜率："+String.format("%.1f", (userInfo[1].getNumWin()*100.0/(userInfo[1].getNumWin()+userInfo[1].getNumLose()+userInfo[1].getNumPeace())))+"%");
+		user2rate.setFont(new Font("华文行楷",Font.CENTER_BASELINE,25));
+		user2rate.setBounds(161,190,300,40);
+		Pane3.add(user2rate);
 		
 		//添加重新开始按钮
 		DiyButton AllReset = new DiyButton("Image\\ButtonAllReset(0).png","Image\\ButtonAllReset(1).png");
-		AllReset.setBounds(780, 610, 326, 115);
+		AllReset.setBounds(1150, 600, 326, 115);
+		MyCanvas.add(AllReset);
 		AllReset.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent arg0){
@@ -157,7 +200,6 @@ public class ChineseChessMainFrame extends JFrame {
 				MyCanvas.paintImmediately(0, 0, MyCanvas.getWidth(), MyCanvas.getHeight());
 			}
 		});
-		//MyCanvas.add(AllReset);
 		Pane1.add(AllReset);
 		
 		//添加时间标签
@@ -171,7 +213,7 @@ public class ChineseChessMainFrame extends JFrame {
 		
 		//添加认输按钮
 		DiyButton WantLose = new DiyButton("Image\\ButtonLose(0).png","Image\\ButtonLose(1).png");
-		WantLose.setBounds(780, 360, 326, 115);
+		WantLose.setBounds(45, 666, 326, 115);
 		WantLose.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent arg0){
@@ -191,12 +233,11 @@ public class ChineseChessMainFrame extends JFrame {
 				
 			}
 		});
-		//MyCanvas.add(WantLose);
 		Pane1.add(WantLose);
 		
 		//添加平局按钮
 		DiyButton WantEqual = new DiyButton("Image\\ButtonEqual(0).png","Image\\ButtonEqual(1).png");
-		WantEqual.setBounds(780, 230, 326, 115);
+		WantEqual.setBounds(345, 666, 326, 115);
 		WantEqual.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent arg0){
@@ -205,12 +246,11 @@ public class ChineseChessMainFrame extends JFrame {
 				userInfo[1].update("peace");
 			}
 		});
-		//MyCanvas.add(WantEqual);
 		Pane1.add(WantEqual);
 		
 		//添加悔棋按钮
 		DiyButton WantBack = new DiyButton("Image\\ButtonBack(0).png","Image\\ButtonBack(1).png");
-		WantBack.setBounds(780, 100, 326, 115);
+		WantBack.setBounds(645, 666, 326, 115);
 		WantBack.addMouseListener(new MouseAdapter(){
 			@Override
 			public void mouseClicked(MouseEvent arg0){
